927a6cd4346118200efa2496764f658b5a4cc55a
luuquang <luungocquang2002@gmail.com>
2022-12-23 21:58:46 +0700

    custom ui

---
+++ BatchRename/InputAddCounter.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BatchRename
+{
+    /// <summary>
+    /// Interaction logic for InputAddCounter.xaml
+    /// </summary>
+    public partial class InputAddCounter : Window
+    {
+        public string Start { get; set; } = "";
+        public string Step { get; set; } = "";
+        public InputAddCounter(string start, string step)
+        {
+            InitializeComponent();
+            Start = start;
+            Step = step;
+            DataContext = this;
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        { 
+            DialogResult = true;
+        }
+    }
+}
---
+++ BatchRename/InputRemoveSpecialCharsRule.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BatchRename
+{
+    /// <summary>
+    /// Interaction logic for InputRemoveSpecialCharsRule.xaml
+    /// </summary>
+    public partial class InputRemoveSpecialCharsRule : Window
+    {
+        public string Data { get; set; } = "";
+        public InputRemoveSpecialCharsRule(string specials)
+        {
+            InitializeComponent();
+            DataContext = this;
+            Data = specials;
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+
+        }
+    }
+}
